6e5209558d4ef8167f8bfe46788ee7f2df93d2d6
Andrew Meyer <AMeyer1987@gmail.com>
2019-08-04 22:25:37 -0400

    Made changes to files required by assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## creates a object that stores a matrix and the matrix inverse for future use
+## instead of recalculating a matrix inverse, the inverse is recalled
 
-## Write a short comment describing this function
+## creates the cached matrix object that is able to 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+   
+   xinv <- NULL
+   set <- function(y){
+      x <<- y
+      xinv <<- NULL
+   }
+   get <- function() x
+   setinv <- function(z) xinv <<- z
+   getinv <- function() xinv
+   list(set = set, get = get,
+        setinv = setinv, getinv = getinv)
+   
 }
 
 
-## Write a short comment describing this function
+## checks to see if inverse has previously been calculated, 
+## and if not returns the inverse of matrix x
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   xinv <- x$getinv()
+   if(!is.null(xinv)) {
+      return(xinv)
+   }
+   data <- x$get()
+   xinv <- solve(data,...)
+   x$setinv(xinv)
+   xinv
+   
 }
